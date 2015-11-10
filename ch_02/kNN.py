d7e2ae84cb33f471808d6213e7229c7eb07826a7
cschenyuan <cschenyuan@gmail.com>
2015-11-11 00:08:33 +0800

    modified kNN.py and add two data set files

--- ch_02/kNN.py
+++ ch_02/kNN.py
@@ -8,6 +8,31 @@ def createDataSet():
     labels = ['A','A','B','B','B']
     return group, labels
 
+def file2matrix (filename):
+    fr = open(filename)
+    arrayOlines = fr.readlines()
+    numberOfLines = len(arrayOlines)
+    returnMat = zeros((numberOfLines,3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOlines :
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
+
+def autoNorm (dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = normDataSet/tile(ranges, (m,1))
+    return normDataSet, ranges, minVals
+
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
 
